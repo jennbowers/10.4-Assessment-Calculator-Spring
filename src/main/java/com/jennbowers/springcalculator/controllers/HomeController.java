0ed5d95a2399ee531da6825dbe56057c27343c6f
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-27 14:16:23 -0400

    Set up form on HTML page, controller is successfully rendering HTML page

--- src/main/java/com/jennbowers/springcalculator/controllers/HomeController.java
+++ src/main/java/com/jennbowers/springcalculator/controllers/HomeController.java
@@ -1,4 +1,15 @@
 package com.jennbowers.springcalculator.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping("/")
+    public String index(){
+        System.out.println("I'm working!!");
+        return "index";
+    }
 }
