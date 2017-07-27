2893f66e0cbbfa84b3af43f56612507195f479c8
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-27 14:57:53 -0400

    Added in RequestParams to pull in inputs from form, and created switch case for performing operations. About to test for success.

--- src/main/java/com/jennbowers/springcalculator/controllers/HomeController.java
+++ src/main/java/com/jennbowers/springcalculator/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.jennbowers.springcalculator.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,8 +9,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
 
     @RequestMapping("/")
-    public String index(){
-        System.out.println("I'm working!!");
+    public String index(@RequestParam(value = "firstOperand", required = false) double firstOperand,
+                        @RequestParam(value = "operatorSelect", required = false) String operatorSelect,
+                        @RequestParam(value = "secondOperand", required = false) double secondOperand){
+        System.out.println(firstOperand);
+        System.out.println(operatorSelect);
+        System.out.println(secondOperand);
+
+        calculator(firstOperand, operatorSelect, secondOperand);
+
         return "index";
+    }
+
+    public void calculator(double firstOperand, String operator, double secondOperand) {
+        double result;
+        switch(operator) {
+            case "add":
+                result = firstOperand + secondOperand;
+                System.out.println(firstOperand + "+" + secondOperand);
+                System.out.println(result);
+                break;
+            case "subtract":
+                result = firstOperand - secondOperand;
+                System.out.println(firstOperand + "-" + secondOperand);
+                System.out.println(result);
+                break;
+            case "multiply":
+                result = firstOperand * secondOperand;
+                System.out.println(firstOperand + "*" + secondOperand);
+                System.out.println(result);
+                break;
+            case "divide":
+                result = firstOperand / secondOperand;
+                System.out.println(firstOperand + "/" + secondOperand);
+                System.out.println(result);
+                break;
+            default:
+                break;
+        }
     }
 }
